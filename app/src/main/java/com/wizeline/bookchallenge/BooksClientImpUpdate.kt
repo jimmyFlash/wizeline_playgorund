f5a61769102f4742f0bd5244a460d8eec8ee1a9b
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2020-03-19 10:29:06 +0200

    - upgrade gradle plugin to 3.6.0 - upgrade kotlin to 1.3.61

--- app/src/main/java/com/wizeline/bookchallenge/BooksClientImpUpdate.kt
+++ app/src/main/java/com/wizeline/bookchallenge/BooksClientImpUpdate.kt
@@ -8,7 +8,6 @@ import java.text.DecimalFormat
 import kotlin.random.Random
 
 // todo create list with presisatnt book ratings
-// todo centralize the dependencies
 class BooksClientImpUpdate : BooksClient {
 
     override fun getTopRatedBooks(): List<Book> {
