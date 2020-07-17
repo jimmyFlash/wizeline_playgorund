92f51bf2484752d71470b0ed050b7257fc84f854
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2020-07-17 12:03:24 +0200

    - upgrade gradle to 6.1.1 gradle plugin to 4.0.0 - add new AS4 databinding and viewbinding configuration - java8 features enabled in build.gradle (app) - add dependency version checker plugin to main project gradle

--- app/src/main/java/com/wizeline/bookchallenge/MyApplication.kt
+++ app/src/main/java/com/wizeline/bookchallenge/MyApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.wizeline.bookchallenge.di.AppComponent
 import com.wizeline.bookchallenge.di.DaggerAppComponent
 
-class MyApplication : Application(){
+open class MyApplication : Application(){
 
     val appComponent : AppComponent by lazy {
 
